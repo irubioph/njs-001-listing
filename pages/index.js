0a80ad6045a8953752b5181c14ce71a1cee3eae4
irubioph <delightsmediaph@gmail.com>
2023-06-24 05:49:22 +0800

    Lesson 3: Adding Other Components

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
    <div>
+    <Navbar />
     <h1>Homepage</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium sequi ea hic. Tempora, veniam repudiandae aperiam maxime ut exercitationem libero hic. Tenetur modi debitis eaque a! Recusandae fuga odio voluptatibus?</p>
+    <Footer />
    </div>
   )
 }
