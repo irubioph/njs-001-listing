9568439e927406a5c20922e94ae1c152f4a11514
irubioph <delightsmediaph@gmail.com>
2023-06-26 08:24:21 +0800

    Lesson 10: Fetching Data (getStaticProps)

--- pages/listing/index.js
+++ pages/listing/index.js
@@ -1,7 +1,26 @@
-const Listing = () => {
+import styles from '@/styles/Listing.module.css'
+
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { listing: data }
+    }
+}
+
+const Listing = ({ listing }) => {
     return ( 
         <div>
-            <h1>All Listing</h1>
+            <h1>Ivan's Listing</h1>
+            {listing.map(listing => (
+                <div key={listing.id}>
+                    <a className={ styles.single }>
+                        <h3>{ listing.name }</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
